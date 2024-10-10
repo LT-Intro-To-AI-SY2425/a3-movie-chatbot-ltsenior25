5133c65bae7d89af5cf13a875e60e3eb67e5d407
ltsenior25 <bepop@cps.edu>
2024-10-10 10:24:35 -0500

    finished!!!!

--- a3.py
+++ a3.py
@@ -244,15 +244,21 @@ def search_pa_list(src: List[str]) -> List[str]:
     """Takes source, finds matching pattern and calls corresponding action. If it finds
     a match but has no answers it returns ["No answers"]. If it finds no match it
     returns ["I don't understand"].
-
     Args:
         source - a phrase represented as a list of words (strings)
-
     Returns:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
     """
-    pass
+    for pat, act in pa_list:
+        val = match(pat, src)
+        if val is not None: 
+            result = act(val)
+            if result:  
+                return result
+            else:
+                return ["No answers"]
+    return ["I don't understand"]
 
 
 def query_loop() -> None:
